ea38b1e399b0b823a19b227494437ec89d6e5ff1
Alexcad <alexcadxx@gmail.com>
2025-12-05 14:50:02 +0300

    Mock of request to server with method GET has been used

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
-// import styles from './app.module.css'
+import { useEffect, useState } from 'react';
+
+import styles from './app.module.css';
+
+const PRODUCTS_MOCK = [
+	{ id: '001', name: 'TV set', price: 39900 },
+	{ id: '002', name: 'Smartphone', price: 18900 },
+	{ id: '003', name: 'Hairdryer', price: 1749 },
+];
 export function App() {
-	return <div>START</div>;
+	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+
+		new Promise((resolve) => {
+			setTimeout(() => {
+				resolve({ json: () => PRODUCTS_MOCK });
+			}, 5000);
+		})
+
+			.then((loadedData) => loadedData.json())
+			.then((loadedProducts) => {
+				setProducts(loadedProducts);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
+	console.log('products', products);
+
+	return (
+		<div className={styles.app}>
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				products.map(({ id, name, price }) => (
+					<div key={id}>
+						{name} - {price} rub
+					</div>
+				))
+			)}
+		</div>
+	);
 }
